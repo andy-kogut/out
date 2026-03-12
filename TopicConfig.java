cd77611fa5d1b78dc1c43dbebe356727443c957f
andy-kogut <72555434+andy-kogut@users.noreply.github.com>
2026-03-12 12:11:12 +0100

    Update TopicConfig.java

--- TopicConfig.java
+++ TopicConfig.java
@@ -1,19 +1,20 @@
+import lombok.Value;
 import java.util.Map;
 
 public class TopicConfig {
 
-    record Route(String origin, String dest) {}
+    @Value
+    static class Route {
+        String origin;
+        String dest;
+    }
 
     private static final Map<String, Route> ROUTES = Map.of(
             "t1", new Route("service1", "service2"),
             "t2", new Route("service3", "service4")
     );
 
-    public static String getOrigin(String topic) {
-        return ROUTES.get(topic).origin();
-    }
-
-    public static String getDest(String topic) {
-        return ROUTES.get(topic).dest();
+    public static Route get(String topic) {
+        return ROUTES.get(topic);
     }
 }
